e1127625f01a9c6b6a74efd1005edbb46eee4eab
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-10-17 15:08:58 +0100

    array_diff

---
+++ array_diff.py
@@ -0,0 +1,8 @@
+def array_diff(a, b):
+    return [x for x in a if x not in b]
+
+
+print(array_diff([1, 2], [1]))
+print(array_diff([1, 2], [2]))
+print(array_diff([1, 2, 2], [1]))
+print(array_diff([1, 2, 2], [2]))
