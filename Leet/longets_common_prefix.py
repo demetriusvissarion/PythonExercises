c89caa4ebc5a4bcb8ddc017617790d27aed22b78
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2024-06-01 13:33:48 +0100

    refactored short solution but less performant

--- Leet/longets_common_prefix.py
+++ Leet/longets_common_prefix.py
@@ -1,33 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs):
-        if strs == [""]:
+        if not strs:
             return ""
-        elif len(strs) == 1:
-            return strs[0]
-        else:
-            # find shortes word and trim all words to same length
-            min_length = min(len(word) for word in strs)
-            trimmed_words = [word[:min_length] for word in strs]
 
-            # take first word
-            longest_common_prefix = trimmed_words[0]
+        shortest_word = min(strs, key=len)
 
-            # compare each letter from first word with the next word
-            for word in trimmed_words[1:]:
-                if longest_common_prefix == '' or word == '':
-                    break
-                if longest_common_prefix[0] != word[0]:
-                    longest_common_prefix = ''
-                    break
-                else:
-                    for number in range(len(longest_common_prefix)):
-                        if longest_common_prefix[number] == word[number]:
-                            continue
-                        else:
-                            longest_common_prefix = longest_common_prefix[:number]
-                            break
+        for i, char in enumerate(shortest_word):
+            for word in strs:
+                if word[i] != char:
+                    return shortest_word[:i]
 
-            return longest_common_prefix
+        return shortest_word
 
 
 solution = Solution()
